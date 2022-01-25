b9cd4d191f68e7120d25407f35fb078ef4459a32
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-01-25 09:32:04 +0000

    Add graph to report

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -8,18 +8,19 @@ write_to_file() {
 	./randGeneoTree "$1" 0.5 > "$INPUT"
 }
 
+rm -f "$OUTPUT"
 touch "$OUTPUT"
 
-for size in $(seq 1 5000 100001); do
+for size in $(seq 1 50000 1000001); do
 	sum_times=0
 
-	for round in $(seq 0 4); do
+	for round in $(seq 0 2); do
 		write_to_file "$size"
 		time=$({ time -p "$EXE" < "$INPUT"; } 2>&1 | grep "real" | awk '{print $2}')
 		sum_times=$(echo "scale=3; ${sum_times} + ${time}" | bc -l)
 	done
 
-	average=$(echo "scale=3; ${sum_times} / 5" | bc -l)
+	average=$(echo "scale=3; ${sum_times} / 3" | bc -l)
 
 	echo "$size $average" >> "$OUTPUT"
 	echo "Finished benchmark for size "$size""
